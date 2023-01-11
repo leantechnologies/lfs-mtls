ae7816c27ceb6f9bf5e493d20f0a955413054dd4
Amr Essam <amr@leantech.me>
2023-01-11 19:41:59 +0400

    Added support for ksa and sandbox and fixed url

--- index.ts
+++ index.ts
@@ -15,20 +15,25 @@ app.use(express.urlencoded({ extended: true }))
 app.all('*', async (req, res) => {
   try {
     const { headers, body, params, query, method, originalUrl } = req
+    const { region, sandbox, mtls } = headers;
+    const mtlsValue = (mtls as string).toLowerCase() === 'true'
+    const sandboxValue = (sandbox as string).toLowerCase() === 'true'
+    const host = `${mtlsValue && sandboxValue ? 'mtls' : ''}${sandboxValue ? '.sandbox' : '.api'}${region === 'ksa' ? '.sa' : ''}.leantech.me`
     delete headers['content-length']
     delete headers['transfer-encoding']
     delete headers['accept']
     delete headers['content-type']
     delete headers['connection']
     delete headers['host']
+    delete headers['region']
+    delete headers['sandbox']
+    delete headers['mtls']
     // @ts-ignore
     const axiosReq = axios({
-      url: `https://api.leantech.me${originalUrl}`,
+      url: `https://${host}${originalUrl}`,
       method: method as Method,
       data: body,
-      params,
-      headers: { ...headers, host: 'api.leantech.me' },
-      query,
+      headers: { ...headers, host },
       httpsAgent: agent,
     })
     const { status, data, headers: responseHeaders } = await axiosReq
